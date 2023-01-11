31471cd3267e678764e3015b7ced733ea4d5d8a5
John Arley Cano <john.cano@devimed.com.co>
2023-01-10 21:30:20 -0500

    style(Footer): Información añadida

--- application/views/core/plantilla1/footer.php
+++ application/views/core/plantilla1/footer.php
@@ -12,27 +12,26 @@
                 <div class="row">
                     <div class="col-12 col-xl-4">
                         <div class="site-footer__widget footer-contacts">
-                            <h5 class="footer-contacts__title">Contact Us</h5>
-                            <div class="footer-contacts__text">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing
-                                elit. Integer in feugiat lorem.
-                            </div>
+                            <h5 class="footer-contacts__title">Contáctanos</h5>
                             <address class="footer-contacts__contacts">
                                 <dl>
-                                    <dt>Phone Number</dt>
-                                    <dd>+1 (800) 060-07-30</dd>
+                                    <dt>Número telefónico</dt>
+                                    <dd>(+57) 310 4114048</dd>
                                 </dl>
                                 <dl>
-                                    <dt>Email Address</dt>
-                                    <dd>red-parts@example.com</dd>
+                                    <dt>Correo electrónico</dt>
+                                    <dd>redesmyfsb@gmail.com</dd>
                                 </dl>
                                 <dl>
-                                    <dt>Our Location</dt>
-                                    <dd>715 Fake Street, New York 10021 USA</dd>
+                                    <dt>Nuestra ubicación</dt>
+                                    <dd>
+                                        <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.google.com%2Fmaps%2Fdir%2F%3Fapi%3D1%26destination%3D6.1596808701549%252C-75.613596439362%26fbclid%3DIwAR0rW0ToH2ZNu2tnS39XxgDgRT6mT2JTusqbSH90n5Ik6ZjRFb3ojrrcuHQ&h=AT0mwKe-JGPM8kVe5CCGYYm_NW9SWRYIPl5jFikYCXDPEwQ4NXSMCpPEUt345qmkNtFx5cwRUi6AvHMk0e1OIEfCyACZwE5omZObp9wFbXe6DpA9qVQSx30ptNIEreVkgNFUYA" target="_blank">calle 31 #41 15 Parqueadero bodegas del Rio 055413 Itagüí, Colombia</a>
+                                    </dd>
                                 </dl>
                                 <dl>
-                                    <dt>Working Hours</dt>
-                                    <dd>Mon-Sat 10:00pm - 7:00pm</dd>
+                                    <dt>Horarios de atención</dt>
+                                    <dd>Lunes a viernes de 07:30am a 06:00pm</dd>
+                                    <dd>Sábados de 07:30am a 04:00pm</dd>
                                 </dl>
                             </address>
                         </div>
@@ -67,7 +66,12 @@
                     </div>
                     <div class="col-12 col-md-6 col-xl-4">
                         <div class="site-footer__widget footer-newsletter">
-                            <h5 class="footer-newsletter__title">Newsletter</h5>
+                            <h5 class="footer-contacts__title">Acerca de nosotros</h5>
+                            <div class="footer-contacts__text">
+                                Somos una empresa líder en el mercado de repuestos con más de 20 años de experiencia. Contamos con excelente respaldo de nuestros proveedores (Cummins, Hércules, Coexito, KTC, Timken, Koyo, Grupo A) entre otros.
+                            </div><br>
+
+                            <!-- <h5 class="footer-newsletter__title">Newsletter</h5>
                             <div class="footer-newsletter__text">
                                 Enter your email address below to subscribe to our newsletter and keep up to date with discounts and special offers.
                             </div>
@@ -75,17 +79,16 @@
                                 <label class="sr-only" for="footer-newsletter-address">Email Address</label>
                                 <input type="text" class="footer-newsletter__form-input" id="footer-newsletter-address" placeholder="Email Address...">
                                 <button class="footer-newsletter__form-button">Subscribe</button>
-                            </form>
-                            <div class="footer-newsletter__text footer-newsletter__text--social">
-                                Follow us on social networks
-                            </div>
+                            </form> -->
+                            <!-- <div class="footer-newsletter__text footer-newsletter__text--social"></div> -->
+                            <h5 class="footer-contacts__title">Síguenos en nuestras redes sociales</h5>
                             <div class="footer-newsletter__social-links social-links">
                                 <ul class="social-links__list">
-                                    <li class="social-links__item social-links__item--facebook"><a href="https://themeforest.net/user/kos9" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-                                    <li class="social-links__item social-links__item--twitter"><a href="https://themeforest.net/user/kos9" target="_blank"><i class="fab fa-twitter"></i></a></li>
-                                    <li class="social-links__item social-links__item--youtube"><a href="https://themeforest.net/user/kos9" target="_blank"><i class="fab fa-youtube"></i></a></li>
-                                    <li class="social-links__item social-links__item--instagram"><a href="https://themeforest.net/user/kos9" target="_blank"><i class="fab fa-instagram"></i></a></li>
-                                    <li class="social-links__item social-links__item--rss"><a href="https://themeforest.net/user/kos9" target="_blank"><i class="fas fa-rss"></i></a></li>
+                                    <li class="social-links__item social-links__item--facebook"><a href="https://www.facebook.com/RepuestosSimonBolivar" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li class="social-links__item social-links__item--twitter"><a href="https://twitter.com/myfsimonbolivar" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                                    <li class="social-links__item social-links__item--youtube"><a href="https://www.youtube.com/@repuestossimonbolivar2617" target="_blank"><i class="fab fa-youtube"></i></a></li>
+                                    <li class="social-links__item social-links__item--instagram"><a href="https://www.instagram.com/repuestossimonbolivar/" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                                    <li class="social-links__item social-links__item--linkedin"><a href="https://www.linkedin.com/company/sim%C3%B3n-bol%C3%ADvar" target="_blank"><i class="fab fa-linkedin"></i></a></li>
                                 </ul>
                             </div>
                         </div>
@@ -97,9 +100,7 @@
             <div class="container">
                 <div class="site-footer__bottom-row">
                     <div class="site-footer__copyright">
-                        <!-- copyright -->
-                        Powered by HTML — Designed by <a href="https://themeforest.net/user/kos9" target="_blank">Kos</a>
-                        <!-- copyright / end -->
+                        Un desarrollo de <a href="https://johnarleycano.com" target="_blank">John Arley Cano</a> - <?php echo "Versión ".exec('git describe --tags'); ?>
                     </div>
                     <div class="site-footer__payments">
                         <img src="<?php echo base_url(); ?>images/payments.png" alt="">
