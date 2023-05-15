346b6dcdf966078ba6d9bd41378a6e36fc5d1697
John Arley Cano <john.cano@devimed.com.co>
2023-05-15 17:44:57 -0500

    fix(General): Archivos pendientes por subir

--- application/views/core/menu_superior/index.php
+++ application/views/core/menu_superior/index.php
@@ -1,7 +1,6 @@
 <!-- Botón del menú para móviles -->
 <?php $this->load->view('core/menu_superior/movil'); ?>
 
-<!-- site__header -->
 <header class="site__header">
     <div class="header">
         <div class="header__megamenu-area megamenu-area"></div>
@@ -42,9 +41,7 @@
                 <a href="wishlist.html" class="indicator__button">
                     <span class="indicator__icon">
                         <svg width="32" height="32">
-                            <path d="M23,4c3.9,0,7,3.1,7,7c0,6.3-11.4,15.9-14,16.9C13.4,26.9,2,17.3,2,11c0-3.9,3.1-7,7-7c2.1,0,4.1,1,5.4,2.6l1.6,2l1.6-2
-C18.9,5,20.9,4,23,4 M23,2c-2.8,0-5.4,1.3-7,3.4C14.4,3.3,11.8,2,9,2c-5,0-9,4-9,9c0,8,14,19,16,19s16-11,16-19C32,6,28,2,23,2L23,2
-z" />
+                            <path d="M23,4c3.9,0,7,3.1,7,7c0,6.3-11.4,15.9-14,16.9C13.4,26.9,2,17.3,2,11c0-3.9,3.1-7,7-7c2.1,0,4.1,1,5.4,2.6l1.6,2l1.6-2 C18.9,5,20.9,4,23,4 M23,2c-2.8,0-5.4,1.3-7,3.4C14.4,3.3,11.8,2,9,2c-5,0-9,4-9,9c0,8,14,19,16,19s16-11,16-19C32,6,28,2,23,2L23,2 z" />
                         </svg>
                     </span>
                 </a>
@@ -58,4 +55,3 @@ z" />
         </div>
     </div>
 </header>
-<!-- site__header / end -->
--- application/views/core/menu_superior/menu.php
+++ application/views/core/menu_superior/menu.php
@@ -1,14 +1,56 @@
 <div class="header__navbar-menu">
     <div class="main-menu">
         <ul class="main-menu__list">
+            <!-- Inicio -->
+            <li class="main-menu__item main-menu__item--submenu--menu main-menu__item--has-submenu">
+                <a href="<?php echo site_url(); ?>" class="main-menu__link">
+                    Inicio
+                </a>
+            </li><!-- Inicio -->
+
+            <!-- Configuración -->
+            <li class="main-menu__item main-menu__item--submenu--menu main-menu__item--has-submenu">
+                <a href="<?php echo site_url(); ?>" class="main-menu__link">
+                    Configuración
+                    <svg width="7px" height="5px">
+                        <path d="M0.280,0.282 C0.645,-0.084 1.238,-0.077 1.596,0.297 L3.504,2.310 L5.413,0.297 C5.770,-0.077 6.363,-0.084 6.728,0.282 C7.080,0.634 7.088,1.203 6.746,1.565 L3.504,5.007 L0.262,1.565 C-0.080,1.203 -0.072,0.634 0.280,0.282 Z" />
+                    </svg>
+                </a>
+                <div class="main-menu__submenu">
+                    <ul class="menu">
+                        <?php if(ENVIRONMENT == 'development') { ?>
+                            <li class="menu__item menu__item--has-submenu">
+                                <a href="#" class="menu__link">
+                                    Sliders
+                                    <span class="menu__arrow">
+                                        <svg width="6px" height="9px">
+                                            <path d="M0.3,7.4l3-2.9l-3-2.9c-0.4-0.3-0.4-0.9,0-1.3l0,0c0.4-0.3,0.9-0.4,1.3,0L6,4.5L1.6,8.7c-0.4,0.4-0.9,0.4-1.3,0l0,0C-0.1,8.4-0.1,7.8,0.3,7.4z" />
+                                        </svg>
+                                    </span>
+                                </a>
+                                <div class="menu__submenu">
+                                    <ul class="menu">
+                                        <li class="menu__item">
+                                            <a href="<?php echo site_url('configuracion/cargar_vista/sliders_inicio'); ?>" class="menu__link">
+                                                Inicio
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </div>
+            </li><!-- Configuración -->
+            
             <li class="main-menu__item main-menu__item--submenu--menu main-menu__item--has-submenu">
                 <a href="<?php echo site_url(); ?>" class="main-menu__link">
                     Home
-                    <!-- <svg width="7px" height="5px">
+                    <svg width="7px" height="5px">
                         <path d="M0.280,0.282 C0.645,-0.084 1.238,-0.077 1.596,0.297 L3.504,2.310 L5.413,0.297 C5.770,-0.077 6.363,-0.084 6.728,0.282 C7.080,0.634 7.088,1.203 6.746,1.565 L3.504,5.007 L0.262,1.565 C-0.080,1.203 -0.072,0.634 0.280,0.282 Z" />
-                    </svg> -->
+                    </svg>
                 </a>
-                <!-- <div class="main-menu__submenu">
+                <div class="main-menu__submenu">
                     <ul class="menu">
                         <li class="menu__item">
                             <a href="index.html" class="menu__link">
@@ -113,9 +155,9 @@
                             </div>
                         </li>
                     </ul>
-                </div> -->
+                </div>
             </li>
-            <!-- <li class="main-menu__item main-menu__item--submenu--menu main-menu__item--has-submenu">
+            <li class="main-menu__item main-menu__item--submenu--menu main-menu__item--has-submenu">
                 <a href="shop-grid-4-columns-sidebar.html" class="main-menu__link">
                     Shop
                     <svg width="7px" height="5px">
@@ -515,7 +557,7 @@
                         </li>
                     </ul>
                 </div>
-            </li> -->
+            </li>
         </ul>
     </div>
 </div>
