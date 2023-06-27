ea1f456ae446dbb006d358cb0ee3676b7e5365df
John Arley Cano <john.cano@devimed.com.co>
2023-06-26 22:47:06 -0500

    feat(Inicio): Búsqueda de repuestos

---
+++ application/views/inicio/buscar_repuestos.php
@@ -0,0 +1,87 @@
+<div class="block-finder block">
+    <div class="decor block-finder__decor decor--type--bottom">
+        <div class="decor__body">
+            <div class="decor__start"></div>
+            <div class="decor__end"></div>
+            <div class="decor__center"></div>
+        </div>
+    </div>
+    <div class="block-finder__image" style="background-image: url('<?php echo base_url(); ?>images/finder-1903x500.jpg');"></div>
+    <div class="block-finder__body container container--max--xl">
+        <div class="block-finder__title">Busque un repuesto para su vehículo</div>
+        <div class="block-finder__subtitle">Seleccione una marca, grupo y línea para buscar un repuesto</div>
+        <form class="block-finder__form">
+            <div class="block-finder__form-control block-finder__form-control--select">
+                <select id="repuesto_marca" aria-label="Marca">
+                    <option value="">Escoja una marca</option>
+                    <?php foreach($this->configuracion_model->obtener('marcas') as $marca) echo "<option value='$marca->id'>$marca->nombre</option>"; ?>
+                </select>
+            </div>
+            <div class="block-finder__form-control block-finder__form-control--select">
+                <select id="repuesto_grupo" aria-label="Grupo" disabled>
+                    <option value="">Escoja un grupo</option>
+                </select>
+            </div>
+            <div class="block-finder__form-control block-finder__form-control--select">
+                <select id="repuesto_linea" aria-label="Línea" disabled>
+                    <option value="">Escoja una línea</option>
+                </select>
+            </div>
+            <button class="block-finder__form-control block-finder__form-control--button" type="submit">Buscar</button>
+        </form>
+    </div>
+</div>
+
+<script>
+    $().ready(() => {
+        $('form').submit(evento => {
+            evento.preventDefault()
+            
+            let filtros = ''
+
+            if($('#repuesto_marca').val()) filtros += `?marca=${$('#repuesto_marca option:selected').text()}`
+            if($('#repuesto_grupo').val()) filtros += `&grupo=${$('#repuesto_grupo option:selected').text()}`
+            if($('#repuesto_linea').val()) filtros += `&linea=${$('#repuesto_linea option:selected').text()}`
+
+            location.href = `${$('#site_url').val()}productos${filtros}`
+        })
+
+        $('#repuesto_marca').change(async() => {
+            $(`#repuesto_grupo`).html('')
+            
+            let grupos = await consulta('obtener', {
+                tipo: 'grupos',
+                marca: $('#repuesto_marca option:selected').text()
+            })
+            
+            $(`#repuesto_grupo`).append(`<option value=''>Seleccione un grupo</option>`)
+
+            $.each(grupos, function(key, grupo){
+                $(`#repuesto_grupo`).append(`
+                    <option value='${grupo.id}'>${grupo.nombre}</option>
+                `)
+            })
+
+            $('#repuesto_grupo').attr('disabled', false)
+        })
+
+        $('#repuesto_grupo').change(async() => {
+            $(`#repuesto_linea`).html('')
+            
+            let lineas = await consulta('obtener', {
+                tipo: 'lineas',
+                marca: $('#repuesto_marca option:selected').text()
+            })
+            
+            $(`#repuesto_linea`).append(`<option value=''>Seleccione una línea</option>`)
+
+            $.each(lineas, function(key, linea){
+                $(`#repuesto_linea`).append(`
+                    <option value='${linea.id}'>${linea.nombre}</option>
+                `)
+            })
+
+            $('#repuesto_linea').attr('disabled', false)
+        })
+    })
+</script>
--- application/views/inicio/index.php
+++ application/views/inicio/index.php
@@ -1,13 +1,17 @@
 <?php $this->load->view('inicio/slider'); ?>
+
+<?php $this->load->view('inicio/buscar_repuestos'); ?>
+<div class="block-space block-space--layout--divider-nl"></div>
+
 <?php $this->load->view('inicio/caracteristicas'); ?>
-
 <div class="block-space block-space--layout--divider-nl"></div>
+
 <?php $this->load->view('inicio/productos_destacados'); ?>
-
 <div class="block-space block-space--layout--divider-nl"></div>
-<?php $this->load->view('inicio/ofertas'); ?>
 
+<?php $this->load->view('inicio/ofertas'); ?>
 <div class="block-space block-space--layout--divider-lg"></div>
+
 <div class="block block-zone">
     <div class="container">
         <div class="block-zone__body">
@@ -16,8 +20,8 @@
         </div>
     </div>
 </div>
-
 <div class="block-space block-space--layout--divider-sm"></div>
+
 <div class="block block-zone">
     <div class="container">
         <div class="block-zone__body">
@@ -26,8 +30,8 @@
         </div>
     </div>
 </div>
-
 <div class="block-space block-space--layout--divider-sm"></div>
+
 <div class="block block-zone">
     <div class="container">
         <div class="block-zone__body">
@@ -36,20 +40,20 @@
         </div>
     </div>
 </div>
-
 <div class="block-space block-space--layout--divider-nl"></div>
+
 <?php $this->load->view('inicio/bloques_banners'); ?>
-
 <div class="block-space block-space--layout--divider-nl"></div>
+
 <?php $this->load->view('inicio/nuevos_productos'); ?>
-
 <div class="block-space block-space--layout--divider-nl"></div>
+
 <?php $this->load->view('inicio/ultimas_noticias'); ?>
-
 <div class="block-space block-space--layout--divider-nl"></div>
-<?php $this->load->view('inicio/marcas'); ?>
 
+<?php $this->load->view('inicio/marcas'); ?>
 <div class="block-space block-space--layout--divider-nl d-xl-block d-none"></div>
+
 <div class="block block-products-columns">
     <div class="container">
         <div class="row">
