98960ea76610ca8592f1e04eafeee501fed5883e
John Arley Cano <john.cano@devimed.com.co>
2023-07-07 16:12:29 -0500

    feat(General): Cambiada la fuente del sistema

--- application/views/inicio/bloques_banners.php
+++ application/views/inicio/bloques_banners.php
@@ -1,28 +1,32 @@
 <div class="block-banners block">
     <div class="container">
         <div class="block-banners__list">
-            <a href="" class="block-banners__item block-banners__item--style--one">
-                <span class="block-banners__item-image"><img src="<?php echo base_url(); ?>images/banners/banner1.jpg" alt=""></span>
-                <span class="block-banners__item-image block-banners__item-image--blur"><img src="<?php echo base_url(); ?>images/banners/banner1.jpg" alt=""></span>
-                <span class="block-banners__item-title">Motor Oils</span>
+            <a href="<?php echo site_url('productos'); ?>" class="block-banners__item block-banners__item--style--one">
+                <span class="block-banners__item-image"><img src="<?php echo base_url(); ?>images/banners/banner1.jpg?<?php echo date('YmdHis'); ?>"></span>
+                <span class="block-banners__item-image block-banners__item-image--blur">
+                    <img src="<?php echo base_url(); ?>images/banners/banner1.jpg?<?php echo date('YmdHis'); ?>">
+                </span>
+                <span class="block-banners__item-title">OUTLET</span>
                 <span class="block-banners__item-details">
                     Synthetic motor oil with free shipping<br>
                     Friction free life guaranteed
                 </span>
                 <span class="block-banners__item-button btn btn-primary btn-sm">
-                    Shop Now
+                    Comprar
                 </span>
             </a>
-            <a href="" class="block-banners__item block-banners__item--style--two">
-                <span class="block-banners__item-image"><img src="<?php echo base_url(); ?>images/banners/banner2.jpg" alt=""></span>
-                <span class="block-banners__item-image block-banners__item-image--blur"><img src="<?php echo base_url(); ?>images/banners/banner2.jpg" alt=""></span>
-                <span class="block-banners__item-title">Save up to $40</span>
+            <a href="<?php echo site_url('productos'); ?>" class="block-banners__item block-banners__item--style--two">
+                <span class="block-banners__item-image"><img src="<?php echo base_url(); ?>images/banners/banner2.jpg?<?php echo date('YmdHis'); ?>"></span>
+                <span class="block-banners__item-image block-banners__item-image--blur">
+                    <img src="<?php echo base_url(); ?>images/banners/banner2.jpg?<?php echo date('YmdHis'); ?>">
+                </span>
+                <span class="block-banners__item-title">Repuestos en promoción</span>
                 <span class="block-banners__item-details">
                     Luxurious interior part for real aesthetes<br>
                     Leather, fabric, ivory and more.
                 </span>
                 <span class="block-banners__item-button btn btn-primary btn-sm">
-                    Shop Now
+                    Comprar
                 </span>
             </a>
         </div>
