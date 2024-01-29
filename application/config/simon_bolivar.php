6bc9003c087d5c75eace7fbacbd07395e67ed3ff
John Arley <johnarleycano@hotmail.com>
2024-01-29 10:55:51 -0500

    style(Footer): Ajuste a horarios

--- application/config/simon_bolivar.php
+++ application/config/simon_bolivar.php
@@ -3,8 +3,8 @@ $config['telefono'] = '310 411 4048';
 $config['email'] = 'tienda@repuestossimonbolivar.com';
 $config['url_facebook'] = 'https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.google.com%2Fmaps%2Fdir%2F%3Fapi%3D1%26destination%3D6.1596808701549%252C-75.613596439362%26fbclid%3DIwAR0rW0ToH2ZNu2tnS39XxgDgRT6mT2JTusqbSH90n5Ik6ZjRFb3ojrrcuHQ&h=AT0mwKe-JGPM8kVe5CCGYYm_NW9SWRYIPl5jFikYCXDPEwQ4NXSMCpPEUt345qmkNtFx5cwRUi6AvHMk0e1OIEfCyACZwE5omZObp9wFbXe6DpA9qVQSx30ptNIEreVkgNFUYA';
 $config['url_whatsapp'] = 'https://wa.link/hn9ljj';
-$config['horario_semana'] = 'Lunes a viernes de 07:30am a 06:00pm';
-$config['horario_fines_semana'] = 'Sábados 7:30 a.m  - 3:30 p.m';
+$config['horario_semana'] = 'Lunes a viernes de 07:30 a.m. a 06:00 p.m.';
+$config['horario_fines_semana'] = 'Sábados 07:30 a.m.  - 3:00 p.m.';
 $config['direccion_girardota'] = "
     <a href='https://maps.app.goo.gl/YRkc3GuXhXRZ6aij9' target='_blank'>
         Doble Calzada Bello Hatillo, 200 metros antes del Peaje El Trapiche | Bomba la Molienda
