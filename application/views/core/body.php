2c24d360626718b59339d9b0828cf605fe4382e2
John Arley Cano Salinas <johnarleycano@hotmail.com>
2024-09-06 21:50:59 +0000

    feat(General): Agregada etiqueta para Bitrix

--- application/views/core/body.php
+++ application/views/core/body.php
@@ -44,5 +44,12 @@
         <script>
             $().ready(() => actualizarCarrito())
         </script>
+
+        <script>
+            (function(w,d,u){
+                var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
+                var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+            })(window,document,'https://cdn.bitrix24.es/b24455241/crm/tag/call.tracker.js');
+        </script>
     </body>
 </html>
