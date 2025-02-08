dc905e2cf0a040670d0aad4322caef209adb4309
John Arley Cano <john.cano@devimed.com.co>
2025-02-08 16:11:13 -0500

    refactor(Ajustes): Agregado rol para visualización de módulo

--- application/views/core/menu_superior/menu.php
+++ application/views/core/menu_superior/menu.php
@@ -31,6 +31,10 @@
                     </a>
                     <div class="main-menu__submenu">
                         <ul class="menu">
+                            <?php if(isset($permisos) && in_array(['configuracion' => 'configuracion_comprobantes_ver'], $permisos)) { ?>
+                                <li class="menu__item">
+                                    <a href="<?php echo site_url("configuracion/comprobantes/ver"); ?>" class="menu__link">
+                                        Comprobantes
                             <?php if(isset($permisos) && in_array(['configuracion' => 'configuracion_recibos_ver'], $permisos)) { ?>
                                 <li class="menu__item">
                                     <a href="<?php echo site_url("configuracion/recibos/ver/2"); ?>" class="menu__link">
