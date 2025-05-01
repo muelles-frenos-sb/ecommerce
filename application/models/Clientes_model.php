5cc832971e99c8fc1a28d66d893305455ad4cc8f
John Arley Cano <johnarleycano@hotmail.com>
2025-05-01 20:50:14 +0000

    style(Configuración - Pagos): Ajustado el orden de los registros

--- application/models/Clientes_model.php
+++ application/models/Clientes_model.php
@@ -141,7 +141,7 @@ Class Clientes_model extends CI_Model {
                 LEFT JOIN auxiliares AS a ON cf.Desc_auxiliar = a.nombre
                 $where
                 $having
-                ORDER BY dias_vencido DESC, dias_expedicion DESC";
+                ORDER BY Fecha_venc DESC, Nro_cuota, Nro_Doc_cruce";
                 
                 if (isset($datos['id']) || isset($datos['Tipo_Doc_cruce']) || isset($datos['Nro_Doc_cruce'])) {
                     return $this->db->query($sql)->row();
