db12baf9eae503b1a95ec9c180864a4ae9e77e03
John Arley Cano <john.cano@devimed.com.co>
2025-06-30 11:30:24 -0500

    refctor(Proveedores): Agregadas validaciones cuando no hay registros

--- application/views/proveedores/cotizaciones/listar_disponibles.php
+++ application/views/proveedores/cotizaciones/listar_disponibles.php
@@ -1,10 +1,28 @@
-<!-- Se obtienen todas las solicitudes de cotización disponibles -->
-<?php $solicitudes_disponibles = $this->proveedores_model->obtener('solicitudes_disponibles'); ?>
+<?php
+// Se obtienen todas las solicitudes de cotización disponibles
+$solicitudes_disponibles = $this->proveedores_model->obtener('solicitudes_disponibles');
+
+if(empty($solicitudes_disponibles)) {
+    echo "
+    <div class='container'>
+        <div class='alert alert-danger alert-lg mb-3 alert-dismissible fade show'>
+            En este momento no hay solicitudes de cotizaciones disponibles. Intenta de nuevo más tarde.
+        </div>
+    </div>
+    ";
+    exit;
+}
+?>
+
 
 <div class="block">
     <div class="container">
         <div class="card flex-grow-1 mt-4">
             <div class="card-body card-body--padding--2">
+                <div class="alert alert-info alert-lg mb-3 alert-dismissible fade show">
+                    Estas son las solicitudes de cotización que tenemos disponibles para tí: Haz clic en el botón <b>Quiero enviar mi cotización</b>:
+                </div>
+
                 <?php foreach ($solicitudes_disponibles as $solicitud) { ?>
                     <div class="vehicles-list__body">
                         <div class="vehicles-list__item">
--- application/views/proveedores/cotizaciones/realizar.php
+++ application/views/proveedores/cotizaciones/realizar.php
@@ -4,7 +4,10 @@ $datos = [
     'nit' => $nit
 ];
 
+// Se obtienen los datos de la solicitud de cotización
 $solicitud_detalle = $this->proveedores_model->obtener('proveedores_maestro_solicitudes_detalle', $datos);
+
+// Se obtiene el detalle de la cotización, en caso de que el proveedor ya la haya hecho
 $cotizacion_detalle = $this->proveedores_model->obtener('proveedores_cotizaciones_detalle', $datos);
 
 echo "<input type='hidden' id='cotizacion_id' value='$cotizacion_id'>";
@@ -14,54 +17,62 @@ echo "<input type='hidden' id='proveedor_nit' value='$nit'>";
 <div class="block">
     <div class="container">
         <div class="block-header__body">
-            <h1 class="block-header__title">Productos solicitados</h1>
-
-            
-        </div>
-
-        <div class="alert alert-primary alert-lg mb-3 alert-dismissible fade show">
-            Indícanos el precio que ofreces al lado de cada producto que tengas disponible. <!-- <a href="">Esta es la lista de marcas</a> -->
+            <h1 class="block-header__title">Productos requeridos en la solicitud de cotización</h1>
         </div>
 
         <div class="card mb-lg-0">
-            <div class="card-body card-body--padding--2">
-                <div class="row" id="contenedor_cotizacion_detalle">
-                    <div class="table-responsive p-3">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Producto</th>
-                                    <th scope="col">Cantidad</th>
-                                    <th scope="col">Precio</th>
-                                </tr>
-                            </thead>
-                            <tbody id="listado_cotizacion_detalle">
-                                <?php 
-                                foreach ($solicitud_detalle as $registro) {
-                                    if ($cotizacion_detalle) {
-                                        $index = array_search($registro->producto_id, array_column($cotizacion_detalle, 'producto_id'));
-                                        if (gettype($index) === "integer") $cotizacion_detalle_id = $cotizacion_detalle[$index]->id;
-                                    }
-                                ?>
-                                    <tr id="listado_producto_<?php echo $registro->id; ?>"
-                                        data-producto-id="<?php echo $registro->producto_id; ?>"
-                                        data-solicitud-detalle-id="<?php echo $registro->id; ?>"
-                                        <?php if (isset($cotizacion_detalle_id)) echo " data-cotizacion-detalle-id='$cotizacion_detalle_id' "?>
-                                    />
-                                        <td><?php echo $registro->producto; ?></td>
-                                        <td><?php echo $registro->cantidad; ?></td>
-                                        <td>
-                                            <input type="number" class="form-control" id="precio_<?php echo $registro->id; ?>" value="<?php if (isset($cotizacion_detalle_id)) echo $cotizacion_detalle[$index]->precio; ?>">
-                                        </td>
-                                    </tr>
-                                <?php } ?>
-                            </tbody>
-                        </table>
-
-                        <button class="btn btn-success btn-lg w-100" onclick="javascript:guardarCotizacionProductos()">Enviar cotización</button>
+            <?php if(empty($solicitud_detalle)) { ?>
+                <div class='container'>
+                    <div class='alert alert-danger alert-lg mb-3 alert-dismissible fade show'>
+                        Para la solicitud de cotización que elegiste, no hay productos disponibles de las marcas que distribuyes.
                     </div>
                 </div>
-            </div>
+            <?php } ?>
+
+            <?php if(!empty($solicitud_detalle)) { ?>
+                <div class="card-body card-body--padding--2">
+                    <div class="alert alert-info alert-lg mb-3 alert-dismissible fade show">
+                        Indícanos el precio que ofreces al lado de cada producto que tengas disponible. <!-- <a href="">Esta es la lista de marcas</a> -->
+                    </div>
+
+                    <div class="row" id="contenedor_cotizacion_detalle">
+                        <div class="table-responsive p-3">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Producto</th>
+                                        <th scope="col">Cantidad</th>
+                                        <th scope="col">Precio</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="listado_cotizacion_detalle">
+                                    <?php 
+                                    foreach ($solicitud_detalle as $registro) {
+                                        if ($cotizacion_detalle) {
+                                            $index = array_search($registro->producto_id, array_column($cotizacion_detalle, 'producto_id'));
+                                            if (gettype($index) === "integer") $cotizacion_detalle_id = $cotizacion_detalle[$index]->id;
+                                        }
+                                    ?>
+                                        <tr id="listado_producto_<?php echo $registro->id; ?>"
+                                            data-producto-id="<?php echo $registro->producto_id; ?>"
+                                            data-solicitud-detalle-id="<?php echo $registro->id; ?>"
+                                            <?php if (isset($cotizacion_detalle_id)) echo " data-cotizacion-detalle-id='$cotizacion_detalle_id' "?>
+                                        />
+                                            <td><?php echo $registro->producto; ?></td>
+                                            <td><?php echo $registro->cantidad; ?></td>
+                                            <td>
+                                                <input type="number" class="form-control" id="precio_<?php echo $registro->id; ?>" value="<?php if (isset($cotizacion_detalle_id)) echo $cotizacion_detalle[$index]->precio; ?>">
+                                            </td>
+                                        </tr>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+
+                            <button class="btn btn-success btn-lg w-100" onclick="javascript:guardarCotizacionProductos()">Enviar cotización</button>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
         </div>
     </div>
 </div>
