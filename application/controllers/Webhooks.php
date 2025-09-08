65682c32aa2c2a5a520dcdc332e5e07b8d3403b0
John Arley Cano <johnarleycano@hotmail.com>
2025-09-08 18:09:34 -0500

    refactor(Webhooks - Pedido): Se envía el email de notificación para pedidos aprobados

--- application/controllers/Webhooks.php
+++ application/controllers/Webhooks.php
@@ -183,11 +183,11 @@ class Webhooks extends MY_Controller {
                 $errores++;
             }
 
-            // Se envía el correo electrónico con la confirmación del pedido (Error o éxito)
-            enviar_email_pedido($recibo);
-
             // Si el pago fue aprobado
             if($wompi_status == 'APPROVED') {
+                // Se envía el correo electrónico con la confirmación del pedido (Error o éxito)
+                enviar_email_pedido($recibo);
+
                 $notas_pedido = substr("- Pedido $recibo->id E-Commerce | Referencia Wompi: $wompi_reference | ID de Transacción Wompi: $wompi_transaction_id- Dirección de entrega: $recibo->direccion_envio | $recibo->email_factura_electronica | $recibo->ubicacion_envio | $recibo->comentarios", 0, 254);
 
                 $recibo_detalle = $this->productos_model->obtener('recibos_detalle', ['rd.recibo_id' => $recibo->id]);
