e02675602595bf2a9a527577017cc3bd7e81ac43
John Arley Cano <john.cano@devimed.com.co>
2025-11-04 21:25:55 -0500

    style(Productos - Detalle): Ajuste a tamaño de botón

--- application/views/productos/botones/producto_detalle.php
+++ application/views/productos/botones/producto_detalle.php
@@ -3,17 +3,19 @@ $producto = $this->productos_model->obtener('productos', ['id' => $datos['id']])
 $item = buscar_item_carrito($producto->id);
 ?>
 
-<div class="product__actions">
-    <?php if(!empty($item)) { ?>
-            <div class="input-number">
-                <input class="input-number__input form-control form-control-lg" type="number" min="1" max="<?php echo $producto->disponible; ?>" value="<?php echo $item['qty']; ?>" disabled>
-                <div class="input-number__add" onClick="javascript:modificarItem('agregar', '<?php echo $item['rowid']; ?>', <?php echo $producto->id; ?>)"></div>
-                <div class="input-number__sub" onClick="javascript:modificarItem('remover', '<?php echo $item['rowid']; ?>', <?php echo $producto->id; ?>)"></div>
-            </div>
-    <?php } else { ?>
+<?php if(!empty($item)) { ?>
+    <div class="product__actions">
+        <div class="input-number">
+            <input class="input-number__input form-control form-control-lg" type="number" min="1" max="<?php echo $producto->disponible; ?>" value="<?php echo $item['qty']; ?>" disabled>
+            <div class="input-number__add" onClick="javascript:modificarItem('agregar', '<?php echo $item['rowid']; ?>', <?php echo $producto->id; ?>)"></div>
+            <div class="input-number__sub" onClick="javascript:modificarItem('remover', '<?php echo $item['rowid']; ?>', <?php echo $producto->id; ?>)"></div>
+        </div>
+    </div>
+<?php } else { ?>
+    <div class="product__actions">
         <div class="product__actions-item product__actions-item--addtocart">
             <button
-                class="btn btn-primary btn-lg btn-block"
+                class="btn btn-primary btn-xl btn-block"
                 onClick="javascript:agregarProducto({
                     id: <?php echo $producto->id; ?>,
                     precio: <?php echo $producto->precio; ?>,
@@ -23,6 +25,5 @@ $item = buscar_item_carrito($producto->id);
                 <i class="fa fa-plus"></i> Agregar al carrito
             </button>
         </div>
-        <div class="product__actions-divider"></div>
-    <?php } ?>
-</div>
+    </div>
+<?php } ?>
