318506f581926e97f0762c549c3b8746d4fb1d77
John Arley Cano <john.cano@devimed.com.co>
2025-11-06 19:52:25 -0500

    feat(Clientes - Crédito): Agregado botón para activar disponibilidad
    Agregado botón para activar disponibilidad del usuario para recibir solicitudes automáticamente

--- application/views/clientes/solicitud_credito/index.php
+++ application/views/clientes/solicitud_credito/index.php
@@ -1,70 +1,74 @@
 <div class="block-header block-header--has-breadcrumb block-header--has-title">
     <div class="container">
         <div class="block-header__body">
-            <h1 class="block-header__title">Gestión de solicitudes de crédito</h1>
+            <h1 class="block-header__title p-2">Gestión de solicitudes de crédito</h1>
+
+            <center>
+                <div class="status-badge text-center status-badge--style--warning mb-3" id="indicador_estado">
+                    <div class="status-badge__body">
+                        <div class="status-badge__text">
+                            <span id="indicador_estado_texto"><?php echo "Disponible para asignación automática de solicitudes de crédito" ?></span>
+                        </div>
+                    </div>
+                </div>
+            </center>
         </div>
     </div>
 </div>
+<div id="contenedor_modal_usuarios_disponibles"></div>
 
 <div class="pl-5 pr-5">
-    <!-- Si tiene permiso para asignar solicitudes de crédito -->
-    <?php if(isset($permisos) && in_array(['clientes' => 'clientes_asignacion_automatica_solicitudes_credito'], $permisos)) { ?>
-        <input type="hidden" id="rol_asignacion_automatica" value="1">
-        
-        <?php
-        $disponibilidad = "En línea";
-        $disponibilidad_clase = "success";
-        ?>        
-    <?php } else {
-        $disponibilidad = "No disponible";
-        $disponibilidad_clase = "warning";
-    } ?>
-
     <a class="btn btn-primary mb-3" href="#" onClick="javascript:listarUsuariosDisponibles()">Ver <span id="usuarios_disponibles"></span> usuarios disponibles</a>
 
-    <div class="status-badge status-badge--style--<?php echo $disponibilidad_clase; ?> product__fit status-badge--has-icon status-badge--has-text">
-        <div class="status-badge__body">
-            <div class="status-badge__icon"><svg width="13" height="13">
-                    <path d="M12,4.4L5.5,11L1,6.5l1.4-1.4l3.1,3.1L10.6,3L12,4.4z" />
-                </svg>
-            </div>
-            <div class="status-badge__text"><?php echo "$disponibilidad para asignación automática de solicitudes de crédito" ?></div>
-            <div class="status-badge__tooltip" tabindex="0" data-toggle="tooltip" title="Part&#x20;Fit&#x20;for&#x20;2011&#x20;Ford&#x20;Focus&#x20;S"></div>
-        </div>
-    </div>
+    <!-- Botones para activarse para asignación automática de solicitudes -->
+    <?php if(isset($permisos) && in_array(['clientes' => 'clientes_asignacion_automatica_solicitudes_credito'], $permisos)) { ?>
+        <!-- Botón para marcar si está disponible -->
+        <input type="hidden" id="usuario_disponible">
+        
+        <a class="btn btn-success mb-3" href="#" id="asignacion_automatica_disponible" onClick="javascript:cambiarEstadoDisponibilidad(true)">
+            <i class="fa fa-check"></i> Marcarse disponible para recibir solicitudes
+        </a>
+
+        <a class="btn btn-danger mb-3" href="#" id="asignacion_automatica_no_disponible" onClick="javascript:cambiarEstadoDisponibilidad(false)">
+            <i class="fa fa-times"></i> Marcarse NO disponible para recibir solicitudes
+        </a>
+    <?php } ?>
     
     <div id="contenedor_solicitudes_credito"></div>
     <div id="contenedor_asignar_usuario"></div>
 </div>
 <div class="block-space block-space--layout--before-footer"></div>
-<div id="contenedor_modal_usuarios_disponibles"></div>
 
-<script>
+<script>    
     class GestorDisponibilidad {
         constructor() {
             this.intervalo = null
             this.iniciar()
         }
-        
+
         iniciar() {
-            // Si no tiene rol para signación automática, se detiene la ejecución
-            if($('#rol_asignacion_automatica').val() != '1') return
-            
-            // Marcar como disponible al cargar
-            this.marcarDisponibilidad(true)
+            // De entrada se inhabilita la opción para marcarse NO disponible
+            $('#asignacion_automatica_no_disponible').hide()
+
+            // Se obtiene la cantidad de usuarios disponibles
+            this.obtenerUsuariosDisponibles()
             
             // Actualizar cada 2 minutos
             this.intervalo = setInterval(() => {
-                this.marcarDisponibilidad(true)
-            }, 120000)
+                // Si ya se puso como disponible, refresca el estado
+                if($('#usuario_disponible').val() == 1) this.marcarDisponibilidad(true)
+                
+                // Se refrescan los usuarios disponibles
+                this.obtenerUsuariosDisponibles()
+            }, 120000) // 5000
 
-            // Marcar como no disponible al cerrar la página
+            // Marcado como no disponible al cerrar la página
             window.addEventListener('beforeunload', () => {
-                this.marcarDisponibilidad(false)
+                this.marcarDisponibilidad(false, true)
             })
         }
         
-        marcarDisponibilidad(disponible) {
+        marcarDisponibilidad(disponible, refrescar = false) {
             fetch(`${$('#site_url').val()}/configuracion/marcar_disponibilidad`, {
                 method: 'POST',
                 headers: {
@@ -73,29 +77,61 @@
                 body: `disponible=${(disponible ? 1 : 0)}`
             })
             .then(response => response.json())
-            .then(resultado => {
+            .then(async resultado => {
                 // Se obtienen los usuarios disponibles
-                this.obtenerUsuariosDisponibles()
-                
-                // console.log(usuariosDisponibles)
-                // console.log('Estado:', resultado.estado)
+                let usuariosDisponibles = await this.obtenerUsuariosDisponibles()
+
+                // Dependiendo de la disponibilidad
+                if(disponible) {
+                    // Se marca el input del usuario disponible
+                    $('#usuario_disponible').val(1)
+                    
+                    // Ocultar y mostrar botones de conectar/desconectar
+                    $('#asignacion_automatica_disponible').hide();
+                    $('#asignacion_automatica_no_disponible').show();
+
+                    // Texto y color del indicador del estado
+                    $("#indicador_estado").removeClass('status-badge--style--warning').addClass('status-badge--style--success')
+                    $("#indicador_estado_texto").text('En línea')
+                } else {
+                    // Se marca el input del usuario no disponible
+                    $('#usuario_disponible').val(0)
+
+                    // Ocultar y mostrar botones de conectar/desconectar
+                    $('#asignacion_automatica_no_disponible').hide()
+                    $('#asignacion_automatica_disponible').show()
+
+                    // Texto y color del indicador del estado
+                    $("#indicador_estado").removeClass('status-badge--style--success').addClass('status-badge--style--warning')
+                    $("#indicador_estado_texto").text('Disponible para asignación automática de solicitudes de crédito')
+                }
+
+                // Si no es para refrescar, muestra mensaje de éxito y log
+                if(!refrescar) {
+                    mostrarAviso('exito', 'Estado actualizado', 30000)
+                    agregarLog(92, (disponible) ? 'Disponible' : 'No disponible')
+                }
             })
         }
 
         obtenerUsuariosDisponibles() {
-            obtenerPromesa(`${$('#site_url').val()}/configuracion/obtener`, {tipo: 'usuarios_disponibles'})
-            .then(usuariosDisponibles => {
-                if(usuariosDisponibles.length) {
-                    $('#usuarios_disponibles').text(usuariosDisponibles.length)
-                }
+            return obtenerPromesa(`${$('#site_url').val()}/configuracion/obtener`, {tipo: 'usuarios_disponibles'})
+            .then(resultado => {
+                // Se actualiza el texto de los usuarios disponibles
+                $('#usuarios_disponibles').text((resultado.length > 0) ? resultado.length : '')
+
+                return resultado
             })
         }
     }
 
-    // Se inicializa el gestor de disponibilidad del usuario
-    // const gestor = new GestorDisponibilidad()
+    // Se instancia el gestor de disponibilidad
+    const gestor = new GestorDisponibilidad()
 
-    cargarAsignarUsuario = (id) => {
+    // Cambio de la disponibilidad al selecionar uno de los botones
+    cambiarEstadoDisponibilidad = estado => gestor.marcarDisponibilidad(estado)
+
+    cargarAsignarUsuario = id => {
         cargarInterfaz('clientes/solicitud_credito/asignar_usuario', 'contenedor_asignar_usuario', {id: id})
     }
 
@@ -103,7 +139,7 @@
         cargarInterfaz('clientes/solicitud_credito/lista', 'contenedor_solicitudes_credito')
     }
 
-    listarUsuariosDisponibles = (id) => {
+    listarUsuariosDisponibles = id => {
         cargarInterfaz('clientes/solicitud_credito/usuarios_disponibles', 'contenedor_modal_usuarios_disponibles')
     }
 
