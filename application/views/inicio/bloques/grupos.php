7314d7fce9be989644d0c1e7d0919c55e9b63c41
John Arley Cano <john.cano@devimed.com.co>
2025-12-12 19:54:37 -0500

    style(eCommerce): Cambiada la imagen del banner Grupos

--- application/views/inicio/bloques/grupos.php
+++ application/views/inicio/bloques/grupos.php
@@ -1,10 +1,10 @@
 <div class="block-zone__card category-card category-card--layout--overlay" onClick="javascript:location.href='<?php echo site_url("productos"); ?>'">
     <div class="category-card__body">
         <div class="category-card__overlay-image">
-            <img srcset="<?php echo base_url(); ?>images/categories/grupos-mobile.jpg 530w, <?php echo base_url(); ?>images/categories/grupos.jpg 305w" src="<?php echo base_url(); ?>images/categories/category-overlay-2.jpg" sizes="(max-width: 575px) 530px, 305px" alt="">
+            <img srcset="<?php echo base_url(); ?>images/categories/grupos_solido-mobile.jpg? 530w, <?php echo base_url(); ?>images/categories/grupos_solido.jpg? 305w" src="<?php echo base_url(); ?>images/categories/category-overlay-2.jpg" sizes="(max-width: 575px) 530px, 305px" alt="">
         </div>
         <div class="category-card__overlay-image category-card__overlay-image--blur">
-            <img srcset="<?php echo base_url(); ?>images/categories/grupos-mobile.jpg 530w, <?php echo base_url(); ?>images/categories/grupos.jpg 305w" src="<?php echo base_url(); ?>images/categories/category-overlay-2.jpg" sizes="(max-width: 575px) 530px, 305px" alt="">
+            <img srcset="<?php echo base_url(); ?>images/categories/grupos_solido-mobile.jpg? 530w, <?php echo base_url(); ?>images/categories/grupos_solido.jpg? 305w" src="<?php echo base_url(); ?>images/categories/category-overlay-2.jpg" sizes="(max-width: 575px) 530px, 305px" alt="">
         </div>
         <div class="category-card__content">
             <div class="category-card__info">
